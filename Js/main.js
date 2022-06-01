cc99863c52dffad0e97789e17e50c092cc57ad40
sws121-max <saurabhsaikumar14196@gmail.com>
2022-06-01 21:19:19 +0530

    first commit

---
+++ Js/main.js
@@ -0,0 +1,93 @@
+const url ='../docs/con1.pdf';
+
+let pdfDoc = null,
+  pageNum =1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+  const scale =1,
+     canvas = document.querySelector('#pdf-render'),
+      ctx = canvas.getContext('2d');
+
+  // Render the page
+  
+  const renderPage = num => {
+      pageIsRendering=true;
+
+      //Get Page
+      pdfDoc.getPage(num).then(page =>{
+          //Set scale
+          const viewport=page.getViewport({scale});
+          canvas.height=viewport.height;
+          canvas.width=viewport.width;
+
+          const renderCtx={
+              canvasContext :ctx,
+              viewport
+          }  
+          page.render(renderCtx).promise.then(() =>{
+            pageIsRendering=false;
+            if(pageNumIsPending!==null){
+                renderPage(pageNumIsPending);
+                pageNumIsPending=null;
+            }
+          }
+
+          );
+      // Output current page
+      document.querySelector('#page-num').textContent = num;
+
+      });
+  };
+
+
+   // Check for pages rendering
+      const queueRenderPage=num =>{
+          if(pageIsRendering){
+              pageNumIsPending = num;
+          }else{
+              renderPage(num);
+          }
+      } 
+  // Shoe Prev Page
+      const showPrevPage=()=>{
+          if(pageNum <=1){
+              return;
+          }
+          pageNum--;
+          queueRenderPage(pageNum);
+      }
+       // Shoe Next Page
+       const showNextPage=()=>{
+        if(pageNum >=pdfDoc.numPages){
+            return;
+        }
+        pageNum++;
+        queueRenderPage(pageNum);
+    }
+
+
+  // Get The Document
+  pdfjsLib.getDocument(url).promise.then(pdfDoc_ =>{
+     pdfDoc = pdfDoc_;
+     
+     document.querySelector('#page-count').textContent=pdfDoc.numPages;
+     renderPage(pageNum)
+  })
+  .catch(err =>{
+      // Display error
+      const div= document.createElement('div');
+      div.className= 'error';
+      div.appendChild(document.createTextNode(err.message));
+      document.querySelector('body').insertBefore(div,canvas);
+      // Remove Top Bar
+      document.querySelector('.top-bar').style.display='none';
+
+  });
+ 
+  // Button Events
+
+  document.querySelector('#prev-page').addEventListener('click',showPrevPage);
+  document.querySelector('#next-page').addEventListener('click',showNextPage);
+
+
